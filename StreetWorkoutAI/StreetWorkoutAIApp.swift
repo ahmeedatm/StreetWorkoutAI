a07bfe7f2f79fb5ccaf9aeb18a494f60d32e071e
Ahmed Atmane <ahmeeatm@gmail.com>
2026-01-17 16:13:00 +0100

    Initial Commit

---
+++ StreetWorkoutAI/StreetWorkoutAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreetWorkoutAIApp.swift
+//  StreetWorkoutAI
+//
+//  Created by Ahmed  on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct StreetWorkoutAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StreetWorkoutAITests/StreetWorkoutAITests.swift
@@ -0,0 +1,16 @@
+//
+//  StreetWorkoutAITests.swift
+//  StreetWorkoutAITests
+//
+//  Created by Ahmed  on 17/01/2026.
+//
+
+import Testing
+
+struct StreetWorkoutAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
